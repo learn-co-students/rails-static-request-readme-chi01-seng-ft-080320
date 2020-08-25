c4f534e048fa4ff9af5feb66ac4b980b20731d87
github username <github email address>
2020-08-25 13:06:12 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'static#about'
+
+  #static route shows the path between two routers that cannot be updated automatically. 
+  #The path is manually updated. If the changes occur at the network side,
+  # we need to update the changing path in the routing table
 end
